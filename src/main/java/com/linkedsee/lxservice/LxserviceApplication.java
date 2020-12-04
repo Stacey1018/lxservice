0c0382a67fc6ab5edf4ddbd5df7b0c4496621f11
zhangyang <2506465056@qq.com>
2020-12-04 17:35:10 +0800

    java项目基本配置完成

---
+++ src/main/java/com/linkedsee/lxservice/LxserviceApplication.java
@@ -0,0 +1,13 @@
+package com.linkedsee.lxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkedsee/lxservice/LxserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedsee.lxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
